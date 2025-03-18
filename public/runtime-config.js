170d4dfa5a8e16dfc84a0984cd51cce40398b9c2
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-03-18 15:14:30 +0900

    fixed some bugs

---
+++ public/runtime-config.js
@@ -0,0 +1,12 @@
+window["runConfig"] = {
+  nodeEnv: "development",
+  backendUrl: "https://localhost:7234",
+  frontendUrl: "https://trustytalents-dev.esynergy.lv",
+  candedateBaseUrl: "https://candidates-trustytalents-dev.esynergy.lv",
+  appDateFormat: "DD/MM/YYYY",
+  latviaLocationSrc:
+    "",
+  uzbekistanLocationSrc:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d843.3409289087402!2d69.20456376721432!3d41.35620207280256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8d88bc70f0b9%3A0xb01a0158a3a86e7c!2sINNO%20Innovative%20Training%20and%20Production%20Technopark!5e0!3m2!1sen!2s!4v1704291182164!5m2!1sen!2s",
+  apiKey: "81b82cc20b17b1a016bdc7611b70c13e70c59290d195f1ffef7f05cd9b0bc110",
+};
