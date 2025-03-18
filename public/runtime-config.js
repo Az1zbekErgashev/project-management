447d119446f3853754c87fab44d37711a21172d2
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-03-18 20:16:39 +0900

    fixed some bigs

---
+++ public/runtime-config.js
@@ -0,0 +1,6 @@
+window["runConfig"] = {
+  nodeEnv: "development",
+  backendUrl: "https://localhost:7234",
+  frontendUrl: "https://trustytalents-dev.esynergy.lv",
+  appDateFormat: "DD/MM/YYYY",
+};
