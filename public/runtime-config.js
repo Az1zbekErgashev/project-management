88ebb0c6ee0e7f5bd0df0362c7a4ea82b0444f61
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-03-24 22:44:26 +0900

    fixed some bugs

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,6 +1,6 @@
 window["runConfig"] = {
   nodeEnv: "development",
-  backendUrl: "https://crm-api.wisestone-u.com",
+  backendUrl: "https://localhost:7234",
   frontendUrl: "https://trustytalents-dev.esynergy.lv",
   appDateFormat: "DD/MM/YYYY",
 };
